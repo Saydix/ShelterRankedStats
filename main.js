8ffba9192616c72be094be2da3e8ba26d8be7ab1
Yumit <Yumit.main@gmail.com>
2023-08-16 13:43:12 +0500

    add parser site 1.1

---
+++ main.js
@@ -0,0 +1,15 @@
+// Получаем кнопку по классу
+const addButton = document.querySelector('.addGame');
+let gameId;
+
+addButton.addEventListener('click', async () => {
+  try {
+    gameId = prompt('Введите URL игры:', '');
+    const response = await fetch(`gameId`);
+    const data = await response.text();
+    
+    console.log(data);
+  } catch (error) {
+    console.error('Ошибка работы с URL:', error);
+  }
+});
