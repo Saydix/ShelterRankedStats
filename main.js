940edba7f8a1c1b3e2dadd599fa90238e9e32fa2
Yumit <Yumit.main@gmail.com>
2023-08-17 18:36:57 +0500

    all again 1.0

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
 console.log('Версия: 1.0');
 
-document.addEventListener('DOMContentLoaded', () => {
-    const addButton = document.querySelector('.addGame');
-    addButton.addEventListener('click', async () => {
-        const gameId = prompt('Введите URL игры:', '');
+const addButton = document.querySelector('.addGame');
+const apiUrl = 'https://shelterstats.glitch.me'; 
 
-        if (gameId) {
-            try {
-                const response = await axios.get(`/fetch-game?gameId=${encodeURIComponent(gameId)}`);
-                const data = response.data;
-                console.log(data);
-            } catch (error) {
-                console.error('Ошибка работы с URL:', error);
-            }
-        } else {
-            console.log('URL игры не был введен');
-        }
-    });
+addButton.addEventListener('click', async () => {
+  const gameId = prompt('Введите URL игры:', '');
+
+  if (gameId) {
+    try {
+        const response = await axios.get(apiUrl, {
+        params: { gameId: encodeURIComponent(gameId) },
+        });
+
+        const $ = cheerio.load(response.data);
+        let text = '';
+        $('#app > div > main > div > main > section > div.game-stats-table >' + 
+        'div:nth-child(1) > div > div > div > div.table >' + 
+        'div:nth-child(2) > div:nth-child(2)').each((i, elem) => {
+            text += `${$(elem).text()}\n`
+        });
+
+        console.log(text);
+    } catch (error) {
+      console.error('Ошибка работы с URL:', error);
+    }
+  } else {
+    console.log('URL игры не был введен');
+  }
 });
