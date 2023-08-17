fd47d6119afe791d73724cdbf52c6423d9980661
Yumit <Yumit.main@gmail.com>
2023-08-17 19:37:11 +0500

    0.5

--- main.js
+++ main.js
@@ -1,27 +1,19 @@
 console.log('Версия: 1.4');
 
 const addButton = document.querySelector('.addGame');
-const apiUrl = 'https://shelterstats.glitch.me'; 
+const apiUrl = 'https://shelterstats.glitch.me';
 
 addButton.addEventListener('click', async () => {
   const gameId = prompt('Введите URL игры:', '');
 
   if (gameId) {
-    console.log('Fetching game data for gameId:', gameId); // Отладочный вывод
+    console.log('Fetching game data for gameId:', gameId);
     try {
-        const response = await axios.get(apiUrl, {
+      const response = await axios.get(apiUrl, {
         params: { gameId: encodeURIComponent(gameId) },
-        });
+      });
 
-        const $ = cheerio.load(response.data);
-        let text = '';
-        $('#app > div > main > div > main > section > div.game-stats-table >' + 
-        'div:nth-child(1) > div > div > div > div.table >' + 
-        'div:nth-child(2) > div:nth-child(2)').each((i, elem) => {
-            text += `${$(elem).text()}\n`
-        });
-
-        console.log('Fetched data:', text); // Отладочный вывод
+      console.log('Fetched data:', response.data);
     } catch (error) {
       console.error('Error fetching game data:', error.message);
     }
