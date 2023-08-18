3ef99f44c43a0f7fb3c5e66ed7834b6c93ef18a3
Yumit <Yumit.main@gmail.com>
2023-08-18 17:15:06 +0500

    0.3

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-console.log('Version: 1.2');
+console.log('Version: 1.3');
 
 const serverUrl = 'https://shelterstats.glitch.me/';
 const targetUrl = 'https://polemicagame.com/game-statistics/197277';
@@ -6,7 +6,7 @@ const targetUrl = 'https://polemicagame.com/game-statistics/197277';
 fetch(`${serverUrl}/getHtml?url=${encodeURIComponent(targetUrl)}`)
   .then(response => response.text())
   .then(data => {
-    console.log(data); // Вывод полученного HTML-кода
+    // console.log(data); // Вывод полученного HTML-кода
   })
   .catch(error => {
     console.error('An error occurred:', error);
