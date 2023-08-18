3b0d2934928b5bd98caeda9767f8b4b9428645e2
Yumit <Yumit.main@gmail.com>
2023-08-18 17:50:36 +0500

    1.0

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-console.log('Version: 1.4');
+console.log('Version: 1.0');
 
 const serverUrl = 'https://shelterstats.glitch.me/';
 const targetUrl = 'https://polemicagame.com/game-statistics/197277';
@@ -6,30 +6,7 @@ const targetUrl = 'https://polemicagame.com/game-statistics/197277';
 fetch(`${serverUrl}/getHtml?url=${encodeURIComponent(targetUrl)}`)
   .then(response => response.text())
   .then(data => {
-    console.log(data, 'End of main'); 
-
-    const playerDataPattern = /"players":\[(.*?)\]/;
-    const playerDataMatch = data.match(playerDataPattern);
-
-    if (playerDataMatch) {
-      const playerDataJson = `[${playerDataMatch[1]}]`;
-      const playerData = JSON.parse(playerDataJson);
-
-      const players = playerData.map(player => ({
-        name: player.username,
-        role: player.role.title,
-        score: player.achievementsSum.points.toFixed(2),
-      }));
-
-      const result = {
-        "Чья победа": playerData[0].w_l === "win" ? "Победа мирных" : "Победа мафии",
-        "Информация по игрокам": players,
-      };
-
-      console.log(result);
-    } else {
-      console.log("Player data not found.");
-    }
+    console.log(data);
   })
   .catch(error => {
     console.error('An error occurred:', error);
