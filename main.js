0f1f6cc880e915c90411ee2e28a27224d55849fc
Yumit <Yumit.main@gmail.com>
2023-08-18 16:45:00 +0500

    0.2

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-console.log('Version: 1.0');
-
-const axios = require('axios');
+console.log('Version: 1.1');
 
 const serverUrl = 'https://shelterstats.glitch.me/';
 const targetUrl = 'https://polemicagame.com/game-statistics/197277';
 
-axios.get(`${serverUrl}/getHtml?url=${encodeURIComponent(targetUrl)}`)
-  .then(response => {
-    console.log(response.data); 
+fetch(`${serverUrl}/getHtml?url=${encodeURIComponent(targetUrl)}`)
+  .then(response => response.text())
+  .then(data => {
+    console.log(data); // Вывод полученного HTML-кода
   })
   .catch(error => {
     console.error('An error occurred:', error);
