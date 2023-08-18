0e7780b6a0ff37b8be4a795806f6b4c38258f165
Yumit <Yumit.main@gmail.com>
2023-08-18 19:13:12 +0500

    1.1

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-console.log('Version: 1.0');
+console.log('Version: 1.1');
 
 const serverUrl = 'https://shelterstats.glitch.me/';
 const targetUrl = 'https://polemicagame.com/game-statistics/197277';
@@ -6,8 +6,26 @@ const targetUrl = 'https://polemicagame.com/game-statistics/197277';
 fetch(`${serverUrl}/getHtml?url=${encodeURIComponent(targetUrl)}`)
   .then(response => response.text())
   .then(data => {
-    console.log(data);
+    // console.log(data);
   })
   .catch(error => {
     console.error('An error occurred:', error);
   });
+
+
+const dummyElement = document.createElement('div');
+dummyElement.innerHTML = data;
+
+const gameDataAttr = dummyElement.querySelector('Gamestats').getAttribute(':game-data');
+const gameData = JSON.parse(gameDataAttr);
+
+const players = gameData.players;
+
+const nicknames = [];
+
+for (const player of players) {
+    const username = player.username;
+    nicknames.push(username);
+}
+
+console.log(nicknames);
