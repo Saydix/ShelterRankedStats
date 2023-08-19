f0586937e925355bff7cac767697378cd6bcacef
Yumit <Yumit.main@gmail.com>
2023-08-19 17:48:11 +0500

    1.2 fix user point

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 console.log('Version: 1.2');
 
+let gameUrl = prompt('Введите ссылку на игру:', 'https://polemicagame.com/game-statistics/197277');
 let serverUrl = 'https://shelterstats.glitch.me';
-const targetUrl = 'https://polemicagame.com/game-statistics/197277';
 
 
 async function fetchData() {
   try {
-    const response = await fetch(`${serverUrl}/getHtml?url=${encodeURIComponent(targetUrl)}`);
+    const response = await fetch(`${serverUrl}/getHtml?url=${encodeURIComponent(gameUrl)}`);
     const data = await response.text();
 
     const dummyElement = document.createElement('div');
@@ -17,21 +17,28 @@ async function fetchData() {
     if (gameDataAttr) {
       const gameData = JSON.parse(gameDataAttr);
       const players = gameData.players.map(player => {
-        return {
-          username: player.username,
-          role: player.role.title,
-          points: player.points,
-          victory: player.w_l === "win" ? "Победа" : "Поражение",
-          winnerCode: gameData.winnerCode === 1 ? "Победа Мирных" : "Победа Мафии"
+        const achievementsSum = player.achievements.reduce((sum, achievement) => {
+          if (typeof achievement.sum === 'number') {
+            return sum + achievement.sum;
+          }
+          return sum;
+          }, 0);
+    
+      return {
+        username: player.username,
+        role: player.role.title,
+        points: achievementsSum, 
+        victory: player.w_l === "win" ? "Победа" : "Поражение",
+        winnerCode: gameData.winnerCode === 1 ? "Победа Мирных" : "Победа Мафии"
         };
       });
-
+    
       return players;
-    } else {
-      throw new Error('Could not find game data attribute');
-    }
+      } else {
+      throw new Error('Не удалось найти атрибут с данными игры');
+      }
   } catch (error) {
-    console.error('An error occurred:', error);
+    console.error('Ошибка обработки:', error);
     return [];
   }
 }
