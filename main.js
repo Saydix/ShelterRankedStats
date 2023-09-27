bc587299c890ef750546b86881db8463165175e4
Yumit <Yumit.main@gmail.com>
2023-09-28 01:54:03 +0500

    2.9

--- main.js
+++ main.js
@@ -155,6 +155,8 @@ async function getData() {
     deleteGameInit();
     editGameButton(data);
 
+    getUpdatedRating(data);
+
     loadingGamesIndicator.style.display = 'none';
     loadingGamesIndicator2.style.display = 'none';
   } catch (error) {
@@ -813,3 +815,43 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+async function getUpdatedRating(data) {
+  const settings = {
+    tau: 0.5,
+    rating: 1500,
+    rd: 200,
+    vol: 0.06,
+  };
+  const ranking = new glicko2.Glicko2(settings);
+
+  const allMatches = [];
+
+  for (const game of data) {
+    const allPlayers = game.allGames.map((player) => {
+      return ranking.makePlayer();
+    });
+
+    const race = glicko2.makeRace(allPlayers.map((player) => [player]));
+
+    const matches = race.getMatches();
+
+    allMatches.push(...matches);
+  }
+
+  ranking.updateRatings(allMatches);
+
+  const updatedRatings = data.map((game) => {
+    return game.allGames.map((player, index) => {
+      const glickoPlayer = allMatches[index].players[0];
+      return {
+        username: player.username,
+        newRating: glickoPlayer.getRating(),
+        newRD: glickoPlayer.getRd(),
+        newVolatility: glickoPlayer.getVol(),
+      };
+    });
+  });
+
+  console.table(updatedRatings);
+  return updatedRatings;
+}
